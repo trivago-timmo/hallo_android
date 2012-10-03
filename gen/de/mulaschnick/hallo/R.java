0cf64f391f58ffddcb5e0e48496a1feb767edbe4
iluvatar <iluvatar@nordhain-gmbh.de>
2012-10-03 17:16:15 +0200

    First working "Hello User" App

--- gen/de/mulaschnick/hallo/R.java
+++ gen/de/mulaschnick/hallo/R.java
@@ -10,10 +10,20 @@ package de.mulaschnick.hallo;
 public final class R {
     public static final class attr {
     }
+    public static final class id {
+        public static final int knopf=0x7f040002;
+        public static final int message=0x7f040000;
+        public static final int name=0x7f040001;
+    }
     public static final class layout {
         public static final int main=0x7f020000;
     }
     public static final class string {
         public static final int app_name=0x7f030000;
+        public static final int beenden=0x7f030005;
+        public static final int hallo=0x7f030003;
+        public static final int vorname_nachname=0x7f030001;
+        public static final int weiter=0x7f030004;
+        public static final int willkommen=0x7f030002;
     }
 }
--- src/de/mulaschnick/hallo/HalloAndroidActivity.java
+++ src/de/mulaschnick/hallo/HalloAndroidActivity.java
@@ -2,9 +2,17 @@ package de.mulaschnick.hallo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HalloAndroidActivity extends Activity
 {
+    private TextView message;
+    private Button knopf;
+    private EditText eingabe;
+    private boolean bFirstClick;
     /**
      * Called when the activity is first created.
      */
@@ -13,5 +21,32 @@ public class HalloAndroidActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        message = (TextView) findViewById(R.id.message);
+        message.setText(R.string.willkommen);
+
+        knopf = (Button) findViewById(R.id.knopf);
+        knopf.setText(R.string.weiter);
+
+        eingabe = (EditText) findViewById(R.id.name);
+        bFirstClick = true;
+
+        knopf.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                if (bFirstClick == true)
+                {
+                    message.setText(getString(R.string.hallo, eingabe.getText()));
+                    knopf.setText(R.string.beenden);
+                    bFirstClick = false;
+                }
+                else
+                {
+                    finish();
+                }
+            }
+        });
     }
 }
